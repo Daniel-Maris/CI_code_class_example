74c087e6072b2a6fd6cc7230c7abda731be448e9
Daniel Maris <d.maris@differ.nl>
2025-02-06 11:23:23 +0100

    add math library and test

---
+++ mymath.py
@@ -0,0 +1,7 @@
+def my_power(a,b):
+    return a**b
+
+def my_root(a,n):
+    return a**(1/n)
+
+
---
+++ test_math.py
@@ -0,0 +1,7 @@
+from mymath import my_power, my_root
+
+def test_power():
+    assert my_power(2,3) == 8
+
+def test_root():
+    assert my_root(9,2) == 3
